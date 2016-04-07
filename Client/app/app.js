20e577d9470468b547923f6e4ba5ded3030d2f15
Hack Reactor Students <accounts@hackreactor.com>
2016-04-07 16:44:17 -0700

    Complete controller template

--- Client/app/app.js
+++ Client/app/app.js
@@ -1,15 +1,21 @@
 angular.module('app', [
   'ngRoute',
+  'app.login',
   'app.start',
   'app.createevent',
-  'app.confirmedlobby',
+  'app.pollresults',
   'app.preferencevote',
   'app.finalaccept',
+  'app.confirmedlobby',
   'app.factories'
   ])
 .config(function($routeProvider, $httpProvider) {
   $routeProvider
     .when('/', {
+      templateUrl: 'app/templates/login.html',
+      controller: 'LoginController'
+    })
+    .when('/start', {
       templateUrl: 'app/templates/start.html',
       controller: 'StartController'
     })
@@ -17,9 +23,9 @@ angular.module('app', [
       templateUrl: 'app/templates/createevent.html',
       controller: 'CreateEventController'
     }) 
-    .when('/confirmedlobby', {
-      templateUrl: 'app/templates/confirmedlobby.html',
-      controller: 'ConfirmedLobbyController'
+    .when('/pollresults', {
+      templateUrl: 'app/templates/pollresults.html',
+      controller: 'PollResultsController'
     })
     .when('/preferencevote', {
       templateUrl: 'app/templates/preferencevote.html',
@@ -29,7 +35,19 @@ angular.module('app', [
       templateUrl: 'app/templates/finalaccept.html',
       controller: 'FinalAcceptController'
     })
+    .when('/confirmedlobby', {
+      templateUrl: 'app/templates/confirmedlobby.html',
+      controller: 'ConfirmedLobbyController'
+    })
     .otherwise({
       redirectTo: '/'
     });
 });
+
+// Insert Authentication here: //////////////////////
+
+
+
+
+
+
