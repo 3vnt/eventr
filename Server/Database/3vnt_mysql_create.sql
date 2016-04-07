62c6a9a7ad041e28d06511c703af24ee5c7fd452
thomasingalls <thomasingalls@gmail.com>
2016-04-07 15:38:11 -0700

    Add modifications to table definitions from https://trello.com/c/0o5n6w8Z

--- Server/Database/3vnt_mysql_create.sql
+++ Server/Database/3vnt_mysql_create.sql
@@ -5,6 +5,7 @@ USE eventr;
 
 CREATE TABLE users (
 	id int NOT NULL,
+	created_at DATETIME NOT NULL,
 	username varchar(100) NOT NULL,
 	email varchar(100) NOT NULL UNIQUE,
 	password varchar(100) NOT NULL,
@@ -18,20 +19,25 @@ CREATE TABLE events_users (
 
 CREATE TABLE events (
 	id int NOT NULL,
-	name varchar(100) NOT NULL,
-	date DATE NOT NULL default = 0,
-	location varchar,
-	total_cost FLOAT,
+	created_at DATETIME NOT NULL,
+	updated_at DATETIME NOT NULL,
+	event_name varchar(100) NOT NULL,
+	event_date DATE NOT NULL,
+	event_location varchar(200),
+	total_cost FLOAT NOT NULL,
+	event_host int NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE users_choices (
 	user_id int NOT NULL,
-	choices_id int NOT NULL
+	choices_id int NOT NULL,
+	vote_time DATETIME NOT NULL
 );
 
 CREATE TABLE questions (
 	id int NOT NULL,
+	created_at DATETIME NOT NULL,
 	text varchar NOT NULL,
 	creator bigint NOT NULL,
 	event_id varchar NOT NULL,
@@ -40,33 +46,47 @@ CREATE TABLE questions (
 
 CREATE TABLE messages (
 	id int NOT NULL,
+	created_at DATE NOT NULL,
 	text varchar(500) NOT NULL,
-	date DATE NOT NULL,
 	PRIMARY KEY (id)
 );
 
 CREATE TABLE users_messages (
-	user_id bigint NOT NULL,
-	message_id bigint NOT NULL
+	user_id int NOT NULL,
+	message_id int NOT NULL
 );
 
 CREATE TABLE choices (
 	id int NOT NULL,
 	text varchar NOT NULL,
+	isWinningChoice bool NOT NULL,
 	question_id varchar NOT NULL,
 	PRIMARY KEY (id)
 );
 
+--- TODO: need to work on the user experience for payments
 CREATE TABLE payment (
 	id int NOT NULL,
+	created_at DATETIME NOT NULL,
+  paid_at DATETIME NOT NULL,
 	value FLOAT NOT NULL,
 	user_id FLOAT NOT NULL,
 	event_id FLOAT NOT NULL,
 	PRIMARY KEY (id)
 );
 
+CREATE TABLE users_users (
+	user_id int NOT NULL,
+	user_id int NOT NULL,
+	created_at DATETIME NOT NULL
+);
+
+
 ALTER TABLE events_users    ADD CONSTRAINT events_users_fk0     FOREIGN KEY (user_id)     REFERENCES users(id);
 ALTER TABLE events_users    ADD CONSTRAINT events_users_fk1     FOREIGN KEY (event_id)    REFERENCES events(id);
+
+ALTER TABLE events          ADD CONSTRAINT events_fk0           FOREIGN KEY (event_host)  REFERENCES users(id);
+
 
 ALTER TABLE users_choices   ADD CONSTRAINT users_choices_fk0    FOREIGN KEY (user_id)     REFERENCES users(id);
 ALTER TABLE users_choices   ADD CONSTRAINT users_choices_fk1    FOREIGN KEY (choices_id)  REFERENCES choices(id);
@@ -82,3 +102,5 @@ ALTER TABLE choices         ADD CONSTRAINT choices_fk0          FOREIGN KEY (que
 ALTER TABLE payment         ADD CONSTRAINT payment_fk0          FOREIGN KEY (user_id)     REFERENCES users(id);
 ALTER TABLE payment         ADD CONSTRAINT payment_fk1          FOREIGN KEY (event_id)    REFERENCES events(id);
 
+ALTER TABLE users_users     ADD CONSTRAINT users_users_fk0      FOREIGN KEY (user_id)     REFERENCES users(id);
+ALTER TABLE users_users     ADD CONSTRAINT users_users_fk1      FOREIGN KEY (user_id)     REFERENCES users(id);
