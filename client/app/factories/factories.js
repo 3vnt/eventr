38e55944996f73b2de11624f727ce9ab0a013701
thomasingalls <thomasingalls@gmail.com>
2016-04-08 16:08:49 -0700

    Update factories with syntax corrections, remove extra modules, add QuestionsFactory

--- client/app/factories/factories.js
+++ client/app/factories/factories.js
@@ -4,19 +4,19 @@ angular.module('app.factories', [])
   var addEvent = function(event) {
     return $http({
       method: 'POST',
-      url: '' ?????????????
+      url: '', //?????????????
       data: event
     });
   }
-});
+})
 
 
-SocketFactory
-MySqlFactory
+// SocketFactory
+// MySqlFactory
 
 
 //Factory for using socket.io]
-.factory('socket',function ($rootScope) {
+.factory('socket', function ($rootScope) {
   var socket = io.connect();
     return {
       on: function (eventName, callback) {
@@ -38,49 +38,38 @@ MySqlFactory
         });
       }
     };
-});
-
-
-
-
-
-.factory('GoalsFactory', function($http) {
-  var addGoal = function(goal) {
-    return $http({
-      method: 'POST',
-      url: '/api/goals', //??????????
-      data: goal
-    });
-  };
-
-  var fetchGoals = function() {
-    return $http({
-      method: 'GET',
-      url: '/api/goals'
-    }).then(function(response) {
-      return response.data;
-    });
-  };
-
-  return {
-    addGoal: addGoal,
-    fetchGoals: fetchGoals
-  };
 })
 
-.factory('WeatherFactory', function($http) {
-  var fetchData = function() {
+
+
+
+
+.factory('QuestionsFactory', function($http) {
+  var addQuestion = function(question) {
     return $http({
-      method: 'GET', 
-      url: '/api/weather'
-      // url: 'http://api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&APPID={' + weatherAPIkey + '}'
-    })
-    .then(function(response) {
-      return response;
+      method: 'POST',
+      url: '/api/questions',
+      data: question
     });
   };
+  
+  var log_some_things = function() {
+    console.log('this for sure works');
+  };
+  
+  // sends two options with a user
+  var sendResponse = function(options) {
+    return $http({
+      method: 'POST',
+      url: '/api/responses',
+      data: options
+    });
+  };
+   
+
   return {
-    fetchData: fetchData
+    addQuestion: addQuestion,
+    sendResponse: sendResponse
   };
 });
 
