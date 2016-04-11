8d5ca7557961abd8f7d114f6550d12584aff5691
Genevieve1722 <gsublette@gmail.com>
2016-04-11 15:22:12 -0700

    Final accept controller, html, factory

--- client/app/controllers/finalaccept.js
+++ client/app/controllers/finalaccept.js
@@ -1,1 +1,11 @@
-angular.module('app.finalAccept', ['app.factories']);
+angular.module('app.finalAccept', ['app.factories'])
+
+.controller('FinalAcceptController', function($scope, socket, $location) {
+  $scope.confirmed = false;
+
+  $scope.confirmParticipant = function() {
+    //someone should check this socket code. I just copied it from elsewhere
+    socket.emit('confirmedParticipant', $scope.confirmed);
+    $location.path('/confirmedlobby');
+  }
+});
