ca6f93bcb37d4f255696348420a345c600b0458f
thomasingalls <thomasingalls@gmail.com>
2016-04-11 15:21:58 -0700

    Update pollResults view with functionality:
    - date, event selection
    - submit button for final choices,
    - css to highlight final choices
    - preliminary indication of distinction between user and host view
    
    TODO:
    - io.connect()
    - send data to server over socket
    - get data from server over socket
    - server-side handling of this view

--- client/app/controllers/pollresults.js
+++ client/app/controllers/pollresults.js
@@ -1,5 +1,50 @@
 angular.module('app.pollResults', ['app.factories'])
 
-.controller('PollResultsController', function($scope, PollResultsFactory) {
+.controller('PollResultsController', function($scope, socket) {
+  // TODO: is this right?
+  io.connect();
+  // TODO: check if user is Host, 
+  //      if is: enable choosy things.
+  //      if not: just show results so far.
+
+  // get poll results from server.
   $scope.pollResults = 'poll results';
+  $scope.showWarning = false;
+  $scope.selectedEventAndDate = {};
+
+
+  // Dummy data
+  // $scope.dateChoices = [
+  //   {date: new Date(), numVotes: 3},
+  //   {date: new Date(), numVotes: 4},
+  //   {date: new Date(), numVotes: 1}
+  // ];
+  // $scope.eventChoices = [
+  //   {event: 'candy store', numVotes: 2},
+  //   {event: 'movies',      numVotes: 3},
+  //   {event: 'skydiving',   numVotes: 5}
+  // ];
+
+  $scope.hostSelectFinalChoice = function(choiceType, choiceObject) {
+    $scope.selectedEventAndDate[choiceType] = choiceObject[choiceType];
+  };
+
+  $scope.toggleClass = function(choiceType, choiceObject) {
+    return $scope.selectedEventAndDate[choiceType] === choiceObject[choiceType];
+  };
+  
+  $scope.sendConfirmation = function (finalChoices) { 
+    // NOTE: check if values are not defined
+    // TODO: Ensure values are date, time.
+    if (finalChoices.event && finalChoices.date) {
+      // NOTE: currently just logs final choices
+      // TODO: should send final choices to server
+      console.log(finalChoices);
+      $scope.showWarning = false;
+      socket.emit
+    } else {
+      $scope.showWarning = true;
+    }
+  };
+  // 
 });
