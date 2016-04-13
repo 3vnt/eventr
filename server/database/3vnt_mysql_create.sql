966731a3343aa2dc25a85275b1eca8f478ad0a3a
thomasingalls <thomasingalls@gmail.com>
2016-04-13 12:47:39 -0700

    Update table definition to include multiple choices for locations, dates, activity names

--- server/database/3vnt_mysql_create.sql
+++ server/database/3vnt_mysql_create.sql
@@ -1,10 +1,17 @@
--- uncomment this line to reset the database
-DROP DATABASE eventr;
+/* to run this file, from the root directory of the project
 
--- uncomment this line for first run; comment out this line after first run
+mysql -u root database < server/database/3vnt_mysql_create.sql 
+
+... include -p if you set a password
+*/
+-- uncomment this line to reset the database
+-- DROP DATABASE eventr;
+
+-- uncomment this line for first run
 -- CREATE DATABASE eventr;
 
 /*--------------------------------------------------------------------------------
+
 before running this file, you can reset your root password by taking the following
   steps (if you forgot your root password)
 
@@ -47,13 +54,31 @@ CREATE TABLE events (
 	id int NOT NULL AUTO_INCREMENT,
 	created_at DATETIME NOT NULL,
 	updated_at DATETIME NOT NULL,
-	event_name varchar(100) NOT NULL,
-	event_date DATE NOT NULL,
-	event_location varchar(200),
 	response_deadline DATE NOT NULL,
-	total_cost FLOAT NOT NULL,
+	total_cost float,
 	event_host int NOT NULL,
+	event_activity varchar(100),
+	event_date DATE,
+	event_location varchar(200),
 	PRIMARY KEY (id)
+);
+
+CREATE TABLE eventActivityOptions (
+	id int NOT NULL AUTO_INCREMENT,
+	event_activity varchar(100) NOT NULL,
+	event_id int NOT NULL
+);
+
+CREATE TABLE eventDateOptions (
+	id int NOT NULL AUTO_INCREMENT,
+	event_date DATE NOT NULL,
+	event_id int NOT NULL
+);
+
+CREATE TABLE eventLocationOptions (
+	id int NOT NULL AUTO_INCREMENT,
+	event_location varchar(200),
+	event_id int NOT NULL
 );
 
 CREATE TABLE users_choices (
@@ -108,25 +133,33 @@ CREATE TABLE users_users (
 );
 
 
-ALTER TABLE events_users    ADD CONSTRAINT events_users_fk0     FOREIGN KEY (user_id)     REFERENCES users(id);
-ALTER TABLE events_users    ADD CONSTRAINT events_users_fk1     FOREIGN KEY (event_id)    REFERENCES events(id);
+ALTER TABLE events_users    			ADD CONSTRAINT events_users_fk0     			FOREIGN KEY (user_id)    		  REFERENCES users(id);
+ALTER TABLE events_users    			ADD CONSTRAINT events_users_fk1     			FOREIGN KEY (event_id)    		REFERENCES events(id);
 
-ALTER TABLE events          ADD CONSTRAINT events_fk0           FOREIGN KEY (event_host)  REFERENCES users(id);
+ALTER TABLE events          			ADD CONSTRAINT events_fk0           			FOREIGN KEY (event_host)  		REFERENCES users(id);
+ALTER TABLE events          			ADD CONSTRAINT events_fk1           			FOREIGN KEY (event_activity)  REFERENCES eventActivityOptions(id);
+ALTER TABLE events          			ADD CONSTRAINT events_fk2           			FOREIGN KEY (event_date)  		REFERENCES eventDateOptions(id);
+ALTER TABLE events          			ADD CONSTRAINT events_fk3           			FOREIGN KEY (event_location)  REFERENCES eventLocationOptions(id);
 
+ALTER TABLE eventActivityOptions	ADD CONSTRAINT eventActivityOptions_fk0		FOREIGN KEY (event_id) 				REFERENCES event(id);
 
-ALTER TABLE users_choices   ADD CONSTRAINT users_choices_fk0    FOREIGN KEY (user_id)     REFERENCES users(id);
-ALTER TABLE users_choices   ADD CONSTRAINT users_choices_fk1    FOREIGN KEY (choices_id)  REFERENCES choices(id);
+ALTER TABLE eventLocationOptions	ADD CONSTRAINT eventLocationOptions_fk0		FOREIGN KEY (event_id) 				REFERENCES event(id);
 
-ALTER TABLE questions       ADD CONSTRAINT questions_fk0        FOREIGN KEY (creator_id)  REFERENCES users(id);
-ALTER TABLE questions       ADD CONSTRAINT questions_fk1        FOREIGN KEY (event_id)    REFERENCES events(id);
+ALTER TABLE eventDateOptions			ADD CONSTRAINT eventDateOptions_fk0				FOREIGN KEY (event_id) 				REFERENCES event(id);
 
-ALTER TABLE users_messages  ADD CONSTRAINT users_messages_fk0   FOREIGN KEY (user_id)     REFERENCES users(id);
-ALTER TABLE users_messages  ADD CONSTRAINT users_messages_fk1   FOREIGN KEY (message_id)  REFERENCES messages(id);
+ALTER TABLE users_choices   			ADD CONSTRAINT users_choices_fk0    			FOREIGN KEY (user_id)     		REFERENCES users(id);
+ALTER TABLE users_choices   			ADD CONSTRAINT users_choices_fk1    			FOREIGN KEY (choices_id)  		REFERENCES choices(id);
 
-ALTER TABLE choices         ADD CONSTRAINT choices_fk0          FOREIGN KEY (question_id) REFERENCES questions(id);
+ALTER TABLE questions       			ADD CONSTRAINT questions_fk0        			FOREIGN KEY (creator_id)  		REFERENCES users(id);
+ALTER TABLE questions       			ADD CONSTRAINT questions_fk1        			FOREIGN KEY (event_id)    		REFERENCES events(id);
 
-ALTER TABLE payment         ADD CONSTRAINT payment_fk0          FOREIGN KEY (user_id)     REFERENCES users(id);
-ALTER TABLE payment         ADD CONSTRAINT payment_fk1          FOREIGN KEY (event_id)    REFERENCES events(id);
+ALTER TABLE users_messages  			ADD CONSTRAINT users_messages_fk0   			FOREIGN KEY (user_id)     		REFERENCES users(id);
+ALTER TABLE users_messages  			ADD CONSTRAINT users_messages_fk1   			FOREIGN KEY (message_id)  		REFERENCES messages(id);
 
-ALTER TABLE users_users     ADD CONSTRAINT users_users_fk0      FOREIGN KEY (user_id)     REFERENCES users(id);
-ALTER TABLE users_users     ADD CONSTRAINT users_users_fk1      FOREIGN KEY (user_id)     REFERENCES users(id);
+ALTER TABLE choices         			ADD CONSTRAINT choices_fk0          			FOREIGN KEY (question_id) 		REFERENCES questions(id);
+
+ALTER TABLE payment         			ADD CONSTRAINT payment_fk0          			FOREIGN KEY (user_id)     		REFERENCES users(id);
+ALTER TABLE payment         			ADD CONSTRAINT payment_fk1          			FOREIGN KEY (event_id)    		REFERENCES events(id);
+
+ALTER TABLE users_users     			ADD CONSTRAINT users_users_fk0      			FOREIGN KEY (user_id)     		REFERENCES users(id);
+ALTER TABLE users_users     			ADD CONSTRAINT users_users_fk1      			FOREIGN KEY (user_id)     		REFERENCES users(id);
